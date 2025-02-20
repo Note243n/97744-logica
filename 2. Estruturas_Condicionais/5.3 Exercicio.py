c26b3774a479c2220d705489da49546159ef4180
Note <tailon.menezes@ba.estudante.senai.br>
2025-02-20 19:17:14 +0000

    Salvando

--- 2. Estruturas_Condicionais/5.3 Exercicio.py
+++ 2. Estruturas_Condicionais/5.3 Exercicio.py
@@ -1,8 +1,21 @@
 import os
 os.system("clear")
 
-Nome = str(input("Informe o seu E'mail: "))
-Senha = int(input("Infirme a sua Senha: "))
+login_cadastrado = "aluno"
+senha_cadastrada = "123"
+
+login_cadastrado = str(input("Informe o seu Email: "))
+senha_cadastrada = int(input("Infirme a sua Senha: "))
+
+if login_cadastrado == login_cadastrado and senha_cadastrada == senha_cadastrada:
+    print("Bem-vindo")
+else:
+    print("Login ou senha invalidados!")
+
+
+
+
+
 
 
 
---
+++ 2. Estruturas_Condicionais/5.4 Exercicio.py
@@ -0,0 +1,27 @@
+import os
+os.system("clear")
+
+idade = int(input("Informe a sua idade: "))
+
+
+if idade < 18 or idade > 65 :
+    print ("Voce não é obrigado a votar!")
+           
+else:
+    print("Voce Deve Votar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
