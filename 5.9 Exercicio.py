7878292fa15d0ff5dfc64372d81121d254843a9e
Note <tailon.menezes@ba.estudante.senai.br>
2025-02-27 17:44:24 +0000

    Salvando

---
+++ 5.9 Exercicio.py
@@ -0,0 +1,24 @@
+import os
+os.system("clear") # LImpeza do terminal
+
+# Solicitando dados para o usuario
+ 
+import os
+os.system("clear")
+
+# Exibindo dados 
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
