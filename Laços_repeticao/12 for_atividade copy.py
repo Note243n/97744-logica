13bd320a44763b6c7e05a6fc65a105234c3f581e
Note <tailon.menezes@ba.estudante.senai.br>
2025-03-15 02:06:37 +0000

    Salvando

--- Laços_repeticao/12 for_atividade copy.py
+++ Laços_repeticao/12 for_atividade copy.py
@@ -1,6 +1,10 @@
 import os
 import time
 os.system('clear')  # LIMPANDO O TERMINAL
+import os
+from random import choice
+from time import sleep 
+os.system('clear')
 Nota = 0
 
 # SOLICITANDO DADOS
@@ -16,34 +20,38 @@ print()
 # MOSTRANDO DADOS
 
 print('Calculando sua media...')
-time.sleep(1)
+sleep(1)
 os.system('clear')
 
 media = Nota /4  # CALCULANDO A MEDIA FINAL
 
 print(f'A sua media é {media}')
-time.sleep(3)
+sleep(3)
 print()
 os.system('clear')
 
 
 if media >= 7:
  print('Aprovado')
-elif media >= 4:
- print('Recuperação')
-elif media < 4:
+ print()
+elif media <= 4:
  print('Reprovado')
+ print()
 else:
  print('Invalido')
- time.sleep(1)
+ print()
+ sleep(3)
  os.system('clear')
+ 
  print('Recarregue a pagina')
  print()
+ sleep(3)
+ os.system('clear')
 
-os.system('clear')
 
 print('Finalizando seção...')
-time.sleep(1)
+print()
+sleep(3)
 os.system('clear')
 
 print('Obrigado por utilizar o programa 3> ')
