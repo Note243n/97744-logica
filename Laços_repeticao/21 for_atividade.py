4a937aff9bba7020f0810104bceb2eab5b30c6d0
Note <tailon.menezes@ba.estudante.senai.br>
2025-03-21 19:27:50 +0000

    salvando

--- Laços_repeticao/21 for_atividade.py
+++ Laços_repeticao/21 for_atividade.py
@@ -2,13 +2,12 @@ from time import sleep
 import os
 os.system('clear')  # LIMPANDO O TERMINAL
 
+
 QUANTIDADE_NOTAS = 3
 soma = 0
-
 for i in range(QUANTIDADE_NOTAS):
     while True:
      nota = float(input(f'Digite a {i + 1}ª nota:'))
                      
      if nota < 0 or nota > 10:
         print('Nota invalida')
-    else
