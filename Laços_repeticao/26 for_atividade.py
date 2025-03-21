b1df80717be2823cfa023e8dd15012fcfd6b8d18
Note <tailon.menezes@ba.estudante.senai.br>
2025-03-21 19:52:12 +0000

    salvando

---
+++ Laços_repeticao/26 for_atividade.py
@@ -0,0 +1,25 @@
+import os
+
+os.system("cls || clear")
+
+Nome = input('digite seu nome: ')
+nota = float(input('digite sua nota: '))
+
+sequencia = 1
+media = 1
+
+while True:
+   repetir = int(input('Deseja adicionar mais uma nota?\n(1) para sim\n (2) para não: '))
+   if repetir == 1:
+      sequencia += 1
+      nota += float(input(f'Digite a sequencia {sequencia}ª nota '))
+      media += 1
+   if nota < 0:
+      break
+   elif repetir == 2:
+      break
+
+print(f'Sua sequencia é {sequencia}')
+print(f'Quantidades de notas encontradas {nota}')
+print(f'Sua media é {nota / media}')
+   
