c65ec76e7038e3347e64e2d02da53f839241ad52
Note <tailon.menezes@ba.estudante.senai.br>
2025-04-03 19:15:37 +0000

    as

--- Funçoes/função_atividade05.py
+++ Funçoes/função_atividade05.py
@@ -2,3 +2,11 @@ import os
 from time import sleep
 os.system('clear || cls')
 
+def transformador_n(numero):
+    if numero % 2 == 0:
+        print('é par')
+    else:
+        print('é impar')
+
+numero_1 = int(input('Digite um numero: '))
+transformador_n(numero_1)
