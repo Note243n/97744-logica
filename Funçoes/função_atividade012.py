693248a8830bb6e2969f5f444a1cd68d4015549b
Note <tailon.menezes@ba.estudante.senai.br>
2025-04-04 18:05:20 +0000

    salvando

--- Funçoes/função_atividade012.py
+++ Funçoes/função_atividade012.py
@@ -2,4 +2,21 @@ import os
 from time import sleep
 os.system('clear || cls')
 
-def 
+
+def contar_pares_impares():
+    par = 0
+    impar = 0
+
+    for i in range(6):
+        n1 = int(input('Digite um numero: '))
+        if n1 % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+    return par, impar
+    
+quantidade_pares, quantidade_impares = contar_pares_impares()
+
+print(f'{quantidade_pares}')
+print(f'{quantidade_impares}')
+        
